d2d135122a8d35a76f10107792e12f4af4f3641e
abed <syed@apisec.ai>
2023-07-07 07:22:29 +0530

    second commit

---
+++ AzureADMSL/AzureADMSL/src/main/java/com/example/AzureADMSL/AzureAdmslApplication.java
@@ -0,0 +1,13 @@
+package com.example.AzureADMSL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureAdmslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureAdmslApplication.class, args);
+	}
+
+}
---
+++ AzureADMSL/AzureADMSL/src/test/java/com/example/AzureADMSL/AzureAdmslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AzureADMSL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureAdmslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
